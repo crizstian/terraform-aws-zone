092100051ed9304c507eaeaced1597c94f59da91
root <cristiano.rosetti@gmail.com>
2023-12-05 17:26:48 +0000

    git push

--- variables.tf
+++ variables.tf
@@ -9,8 +9,10 @@ variable "vpc_id" {
   default = "vpc-51edc228"
 }
 variable "vpc_subnets" {
+  type    = list(string)
   default = ["subnet-682a8223", "subnet-bef497e4"]
 }
 variable "vpc_security_group_ids" {
+  type    = list(string)
   default = ["sg-e508239b"]
 }
